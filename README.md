211531d0dd25f8062bf378b46f0818d41bd92553
zhangdehao087 <zhangdehao087@qq.com>
2026-06-03 21:58:33 +0800

    V1:新建ex01目录，C语言基础HelloWorld程序

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # zhangdehao666 C语言作业
-V1版本：C语言基础程序，使用printf打印HelloWorld，无键盘输入功能
-V2版本：新增scanf函数，支持键盘自定义输入并回显内容
+V1版本：C语言基础程序，printf打印HelloWorld，无键盘输入功能
--- ex01/main.c
+++ ex01/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-    char str[100];
-    printf("请输入一段文字：");
-    scanf("%s",str);
-    printf("你输入的内容：%s\n",str);
+    printf("Hello World\n");
     return 0;
 }
